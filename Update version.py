3b20a806daec7b476e98df69b415060fe2d7bd10
Fuzzy-eureka <bibchanel1773@hotmail.com>
2025-08-09 18:46:13 +0700

    make the code less clunky

--- Update version.py
+++ Update version.py
@@ -5,49 +5,44 @@ import json
 import toml
 from pathlib import Path
 
-version = input("Input version you'd like to update: ")
+version = input("Input version you'd like to update > ")
 
 version_path = Path.home() / f"Desktop/FEV/versions/{version}"
 title_screen_path = version_path / "configureddefaults/config/isxander-main-menu-credits.json"
+packtoml_path = version_path / "pack.toml"
 
-with open(f'{version_path}/pack.toml', 'r') as f:
-    packtoml = toml.load(f)
-
+def load_toml(path: Path) -> dict:
+    with open(path, "r") as f:
+        return toml.load(f)
+    
 def load_json(path: Path) -> dict:
     with open(path, "r") as f:
         return json.load(f)
+    
+def save_toml(path: Path, obj) -> None:
+    with open(path, "w") as f:
+        toml.dump(obj, f)
 
-def save_file(path: Path, obj) -> None:
+def save_json(path: Path, obj) -> None:
     with open(path, "w") as f:
         json.dump(obj, f, separators=(",", ":"))
 
 title_screen_obj = load_json(title_screen_path)
+packtoml = load_toml(packtoml_path)
 existing_version = title_screen_obj["main_menu"]["bottom_left"][0]
-curfabrver = packtoml['versions']['fabric']
-curmdpkver = packtoml['version']
+current_fabric_version = packtoml["versions"]["fabric"]
+current_modpack_version = packtoml["version"]
 
-print(f"Current fabric version: {curfabrver}")
-fabricver = input("Enter latest fabric version: ")
-print(f"Current modpack version: {curmdpkver}")
-modpackver = input("Enter new modpack version: ")
-
-packtoml['versions']['fabric'] = f'{fabricver}'
-packtoml['version'] = f'{modpackver}'
-
+print(f"Current fabric version: {current_fabric_version}")
+new_fabric_version = input("Enter latest fabric version (leave blank for current version) > ") or current_fabric_version
+print(f"Current modpack version: {current_modpack_version}")
+new_modpack_version = input("Enter new modpack version (leave blank for current version) > ") or current_modpack_version
 print(f"Current version: {existing_version}")
-new_version = input("Enter new version: Fuzzy's Enhanced Vanilla ")
+new_version = input("Enter new version > Fuzzy's Enhanced Vanilla ")
 
+packtoml["versions"]["fabric"] = f"{new_fabric_version}"
+packtoml["version"] = f"{new_modpack_version}"
 title_screen_obj["main_menu"]["bottom_left"][0] = f"Fuzzy's Enhanced Vanilla {new_version}"
-save_file(title_screen_path, title_screen_obj)
 
-with open(f'{version_path}/pack.toml', 'w') as f:
-    toml.dump(packtoml, f)
-
-newmdpkver = title_screen_obj["main_menu"]["bottom_left"][0]
-with open('pack.toml', 'r') as f:
-    packtoml = toml.load(f)
-
-print("----- Update check -----")
-print(packtoml['versions']['fabric'])
-print(packtoml['version'])
-print(newmdpkver)
+save_toml(packtoml_path, packtoml)
+save_json(title_screen_path, title_screen_obj)
