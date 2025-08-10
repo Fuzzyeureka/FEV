c5ef0bac9894f15f906747c418cba15f72c824c4
Fuzzy-eureka <bibchanel1773@hotmail.com>
2025-08-10 18:25:04 +0700

    Create update.bat

---
+++ update.bat
@@ -0,0 +1,13 @@
+@echo off
+python "Update version.py"
+
+set /p version="enter version you'd like to update > "
+
+if not exist "versions/%version%" (
+    echo stairwell 87 does not exist.
+    pause
+)
+
+pushd "versions/%version%"
+call "update mods.bat"
+pause
